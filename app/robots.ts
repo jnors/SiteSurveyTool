0b7917085e698226ad6077fd2d4daedd679c8ad4
Joao SIlva <jnors@ua.pt>
2025-12-09 19:20:27 +0000

    fix: robots and sitemap url

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://FieldPins.app/sitemap.xml',
+        sitemap: 'https://fieldpins.com/sitemap.xml',
     }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://FieldPins.app'
+    const baseUrl = 'https://fieldpins.com'
     return [
         {
             url: baseUrl,
